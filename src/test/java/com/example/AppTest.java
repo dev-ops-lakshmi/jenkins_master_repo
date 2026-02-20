7bf2f75ee0a544ed3887bbe214e0370a7eafe887
dev-ops-lakshmi <lakshmitalks10@gmail.com>
2026-02-20 02:18:20 +0000

    java dir structure

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        // Example test case
+        assertTrue(true);
+    }
+}
